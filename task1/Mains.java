f42a4a6a79016a82b6cd36d64114a07be90eb651
Sagynov Ryskul <ryskulsagynov@gmail.com>
2022-12-22 17:35:13 +0600

    first init

---
+++ task1/Mains.java
@@ -0,0 +1,17 @@
+package kz.attractor.java;
+
+public class Mains {
+
+    public static void main(String[] args) {
+
+        var cats = Cat.makeCats(10);
+        Printer.print(cats);
+
+        // А сюда добавьте код, который будет сортировать коллекцию котов
+        // используйте лямбда-выражения и ссылки на методы
+        //cats.sort(?);
+        //Printer.print(cats);
+    }
+
+
+}
